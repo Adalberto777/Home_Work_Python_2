5d143b778e1a5b13b1adf82d3e5976f7c2e0a0af
Adalberto777 <albert.tliashev@gmail.com>
2022-10-13 23:18:59 +0500

    task5

--- task_5.py
+++ task_5.py
@@ -2,6 +2,16 @@
 import random
 
 
+def mix_str(str):
+    str_mod = []
+    str_find = 0
+    for i in range(len(str)):
+        while str_mod.count(str_find) != 0:
+            str_find = random.randint(0, len(str)-1)
+        str_mod.append(str_find)
+    return str_mod  
+      
+
 str = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-random.shuffle(str)
 print(str)
+print(mix_str(str))
